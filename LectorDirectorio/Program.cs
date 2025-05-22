386f55779fe59b323e546580305c4e2eba6c41f8
FacundoVel <facundo.vel99@gmail.com>
2025-05-22 14:59:56 -0300

    bucle para mostrar carpetas y archivos de la misma

--- LectorDirectorio/Program.cs
+++ LectorDirectorio/Program.cs
@@ -2,13 +2,27 @@
 using System.IO;
 
 Console.WriteLine("Ingrese el directorio a analizar: ");
-string path = Console.ReadLine();
-while(!Directory.Exists(path))
+string? path = Console.ReadLine();
+while (!Directory.Exists(path))
 {
     Console.WriteLine("El directorio NO existe");
     Console.WriteLine("Ingrese directorio valido: ");
     path = Console.ReadLine();
 }
 
-string[] datos = Directory.GetDirectories(path);
-Console.WriteLine(datos);
+string[] files = Directory.GetFiles(path);
+string[] directories = Directory.GetDirectories(path);
+
+foreach (string carpeta in directories)
+{
+    DirectoryInfo info1 = new DirectoryInfo(carpeta);
+    Console.WriteLine($"\nnombre directorio: {info1.Name}");
+    foreach (string archivo in files)
+    {
+        Console.WriteLine("Archivos encontrados: ");
+        FileInfo info = new FileInfo(archivo);
+        Console.WriteLine($"\nnombre: {info.Name} tamaño: {info.Length}");
+
+    }
+}
+
