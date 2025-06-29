ff64b6e867504da9557e6df6944c783fc33ac444
Facundo <facundo.vel99@gmail.com>
2025-06-29 15:18:32 -0300

    Se adapta codigo para el ingreso y control de path

--- LectorDirectorio/Program.cs
+++ LectorDirectorio/Program.cs
@@ -3,26 +3,11 @@ using System.IO;
 
 Console.WriteLine("Ingrese el directorio a analizar: ");
 string? path = Console.ReadLine();
-while (!Directory.Exists(path))
+
+while(!Directory.Exists(path))
 {
     Console.WriteLine("El directorio NO existe");
-    Console.WriteLine("Ingrese directorio valido: ");
+    Console.WriteLine("Ingrese un directorio valido: ");
     path = Console.ReadLine();
-}
-
-string[] files = Directory.GetFiles(path);
-string[] directories = Directory.GetDirectories(path);
-
-foreach (string carpeta in directories)
-{
-    DirectoryInfo info1 = new DirectoryInfo(carpeta);
-    Console.WriteLine($"\nnombre directorio: {info1.Name}");
-    foreach (string archivo in files)
-    {
-        Console.WriteLine("Archivos encontrados: ");
-        FileInfo info = new FileInfo(archivo);
-        Console.WriteLine($"\nnombre: {info.Name} tamaño: {info.Length}");
-
-    }
 }
 
